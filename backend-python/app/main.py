1ae01e303038bfa9d219c8b916df7a8ee991fbe3
chrisedwards551 <chrisedwards551@gmail.com>
2026-07-08 00:43:54 +0000

    Initialize Alembic migrations and remove automatic schema creation

--- backend-python/app/main.py
+++ backend-python/app/main.py
@@ -1,14 +1,8 @@
 from fastapi import FastAPI
 
-from app.database import Base, engine
-import app.models
 
 app = FastAPI()
 
-
-@app.on_event("startup")
-def startup():
-    Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
