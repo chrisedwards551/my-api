4f7a74775b25dbf1c0442ecde9c57bed321d1ed9
chrisedwards551 <chrisedwards551@gmail.com>
2026-07-17 15:06:51 +0000

    Connect Express backend to PostgreSQL database

--- backend-node/index.js
+++ backend-node/index.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const pool = require('./src/config/database');
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello from your Ubuntu VM API!');
+});
+
+app.get('/db-test', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()');
+        
+        res.json({
+            message: 'Database connected!',
+            time: result.rows[0]
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: 'Database connection failed',
+            error: error.message
+        });
+    }
 });
 
 app.listen(3000, () => {
