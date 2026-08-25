09fb6f860c8f37b65d04012e3258b50b05e305ba
chrisedwards551 <chrisedwards551@gmail.com>
2026-08-25 16:32:29 +0000

    Implemented Docker containerized backend infrastructure and service orchestration

--- backend-node/index.js
+++ backend-node/index.js
@@ -45,6 +45,6 @@ app.get('/db-test', async (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Node API running on port 3000');
 });
