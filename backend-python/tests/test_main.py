1037855ebb9697b3ebec75ab066dcbc9198eab83
chrisedwards551 <chrisedwards551@gmail.com>
2026-09-02 17:28:12 +0000

    Test CI failure handling

--- backend-python/tests/test_main.py
+++ backend-python/tests/test_main.py
@@ -1,7 +1,7 @@
 def test_root(client):
     response = client.get("/")
 
-    assert response.status_code == 200
+    assert response.status_code == 201
 
     assert response.json() == {
         "message": "API is running"
